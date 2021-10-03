738c31c77c3d645499943714009187a294887993
miyachu <79947589+miyachu@users.noreply.github.com>
2021-10-03 18:34:36 +0800

    Update install-thrift.sh

--- travis/install-thrift.sh
+++ travis/install-thrift.sh
@@ -13,7 +13,9 @@ git clone -b 0.11.0 https://github.com/apache/thrift.git thrift-0.11.0
 cd thrift-0.11.0
 ./bootstrap.sh
 ./configure --with-cpp=yes --with-c_glib=no --with-java=no --with-ruby=no --with-erlang=no --with-go=no --with-nodejs=no
-make -j2 && sudo make install
-cd lib/py
-sudo python3 setup.py install
-cd ../../..
+#make -j2 && sudo make install
+#cd lib/py
+#sudo python3 setup.py install
+#cd ../../..
+
+cd ../..
